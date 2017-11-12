8ead40b5d849a32a50a6b410fc355c15d8511141
Jason Gruenhagen <jasonmgru@gmail.com>
2017-11-12 10:18:19 -0600

    created entire project

--- README.md
+++ README.md
@@ -9,4 +9,12 @@
 ## Mission
 With the recent string of allegations and convictions of sexual assault in the film industry, viewers need to know whether the movies and tv they are consuming ends up supporting sexual assailants and predators. 
 
-Hollywood job openings provides a quick search to make sure the viewer's tv show or movie is ethical.
+Hollywood Harassment provides a quick search to make sure the viewer's tv show or movie is ethical.
+
+DISCLAIMER: Our algorithm is certainly not 100%
+
+## Resources
+To retrieve and verify our data, we used:
+* OMDb API http://www.omdbapi.com/
+* New York Times article_search API https://developer.nytimes.com/
+* Countless Stack Overflow examples
---
+++ script.js
@@ -0,0 +1,219 @@
+
+/* WEBSITE DATA */
+const badWords = ["abuse", "nonconsensual", "rape", "assault", "grope", "misconduct",
+"allegation", "groping", "harass", "non consen", "offender", 
+"inappropriate", "child groom", "bestiality", "public indec"];
+
+var accused = {};
+
+/* HELPER FUNCTIONS */
+ 
+function purifyNames(names){
+
+    let helper = function(name){
+        var purified = '';
+        
+        for(i=0; i<name.length; i++){
+          if (name[i] === '('){
+            if(name[i-1] === ' '){
+              purified = purified.trim();
+            }
+            
+            return purified;
+          }
+          
+          purified = purified + name[i];
+        }
+    
+        return purified;
+    }
+    
+    var pure = [];
+    $.each(names, function(i, el){
+      pure.push(helper(el));
+    });
+    
+    return pure;
+}
+function betterSplit(names){
+    
+    var nameList = [];
+    var individual = '';
+
+    for(i=0; i<names.length; i++){
+
+        if(names[i] == ','){
+            nameList.push(individual);
+            individual = '';
+        } else {
+            individual = individual + names[i];
+        }
+    }
+    
+    nameList.push(individual);
+
+    for(k=0; k<nameList.length; k++){
+        nameList[k] = nameList[k].trim();
+    }
+
+    return nameList;
+}
+function dedupe(names){
+    var uniqueNames = [];
+    $.each(names, function(i, el){
+        if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
+    });
+
+    return uniqueNames;
+}
+function removeNA(names){
+    return names.filter(function(word){
+        return(word.toLowerCase() !== "n/a");
+    });
+}
+
+
+/* API ACCESS */
+
+function isAccused(articles, person){    
+
+    var ret = false;
+
+    for(i=0; i<articles.length; i++){
+        var headline = articles[i].headline.main;
+
+        for(k=0; k<badWords.length; k++){
+            if(headline.toLowerCase().includes(badWords[k])){
+                k = badWords.length;
+                ret = true;
+
+                //adds DOM node to accusedArticles
+                var dest = articles[i].web_url;
+                var $link = $('<a target=_blank href='+dest+'>'+headline+'</a>');
+                if (accused[person] === undefined) accused[person] = [];
+                accused[person].push($link);
+            }
+        }
+    }
+
+    return ret;
+}
+
+function updateDisplay() {
+    var $disp = $('#display');
+    var $p;
+    var $label;
+
+    $('.loader').hide();
+
+    if($.isEmptyObject(accused)){
+        $label = $('<label id="green">Good to Go!</label>');
+    } else {
+        $label = $('<label id="red">Accusations</label>');
+    }
+    $disp.append($label);
+
+    console.log('accused: '+accused);
+
+    $.each(accused, function(i, el){
+        $label = $('<label>'+i+'</label>');
+        $disp.append($label);
+
+        for(k=0; k<el.length; k++){
+            $p = $('<p></p>');
+            $p.append(el[k]);
+            $disp.append($p);            
+        }
+    });
+
+    $disp.fadeIn(250);
+}
+
+function lookUp(people){
+
+    if(people.length === 0) {
+        updateDisplay();
+        return;
+    }
+
+    var person = people[0];
+    var query = 'headline:"'+person+'"';
+
+    var place = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    place += '?' + $.param({
+        'api-key': "4ea1957978af4a3d8fc042284cf13296",
+        'fq': query
+    });
+
+    $.ajax({
+        url: place,
+        dataType: 'json',
+        method: 'GET',
+    }).done(function(result){
+
+        var articles = result.response.docs;
+
+        isAccused(articles, person);
+        setTimeout(function(){ lookUp(people.slice(1)); }, 1000);
+
+    }).fail(function(err){
+        alert('failure');
+    });
+}
+
+function getPeople(movie){
+    
+    console.log(movie);
+    $.getJSON('https://www.omdbapi.com/?apikey=e3fb366f&t=' + encodeURI(movie)).then(function(response){
+        
+        if(response.Response === "False"){
+            alert("Content Not Found");
+        } else {
+            var director = response.Director;
+            var writer = response.Writer;
+            var actors = response.Actors;
+            console.log(director);
+            console.log(writer);
+            console.log(actors);
+
+            var directorList = betterSplit(director);
+            var writerList = betterSplit(writer);
+            var actorList = betterSplit(actors);
+
+            var people = directorList.concat(writerList.concat(actorList));
+            console.log(people);
+            people = dedupe(purifyNames(removeNA(people)));
+            console.log(people);
+
+            lookUp(people);
+        }
+
+    });
+}
+
+/* EVENTS */
+
+$(document).keypress(function(key){
+    if(key.keyCode === 13){
+
+        reset();
+        createLoader();
+
+        var input = document.getElementById('movieInput');
+        getPeople(input.value);
+    }
+});
+
+function reset(){
+    accused = {};
+
+    $('#display').fadeOut(0);
+    $('#display').empty();
+}
+
+function createLoader(){
+    var loader = $('<div class="loader"></div>');
+    loader.fadeOut(0);
+    $('body').append(loader);
+    loader.fadeIn(500);
+}
